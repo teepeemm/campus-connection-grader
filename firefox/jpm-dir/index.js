aa685edb4451c0148bafefbf79694b53bf305aa6
teepeemm <teepeemm@gmail.com>
2015-06-24 09:44:14 -0500

    started jpm

--- firefox/jpm-dir/index.js
+++ firefox/jpm-dir/index.js
@@ -1,3 +1,5 @@
+// min ff version: 34
+
 var ToggleButton = require('sdk/ui/button/toggle').ToggleButton;
 //var { ToggleButton } = require('sdk/ui/button/toggle');
 var pageMod = require("sdk/page-mod");
@@ -73,3 +75,15 @@ function register(site) {
 	}
     };
 }
+
+
+// the default stuff
+var self = require('sdk/self');
+
+// a dummy function, to show how tests work.
+// to see how to test this function, look at test/test-index.js
+function dummy(text, callback) {
+  callback(text);
+}
+
+exports.dummy = dummy;
